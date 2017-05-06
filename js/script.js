1bc15714d6d5ac7c31e15b061db001185da25a18
ManfredHu <manfredhufe@gmail.com>
2017-05-06 20:25:04 +0800

    Site updated: 2017-05-06 20:25:03

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
 (function($) {
+    // Highlight current nav item on HomePage
+    var winPathName = window.location.pathname;
+    if ($('#navigater-list').length > 0) {
+        $('#navigater-list > a').each(function() {
+            if (winPathName.indexOf($(this).attr('href')) != -1) {
+                $(this).addClass('main-nav-link__active curpoint-normal');
+            }
+        });
+    }
+    // Highlight current nav item on achieve Page
+    if ($('.nav').length > 0) {
+        $('.nav a').each(function() {
+            var currentHref = $(this).attr('href');
+            //匹配且不为链接
+            if (winPathName.indexOf(currentHref) != -1 && currentHref != '/') {
+                $(this).addClass('main-nav-link__active curpoint-normal');
+            } else {
+                $(this).removeClass('main-nav-link__active');
+            }
+        });
+        if ($('.main-nav-link__active').length == 0) {
+            $('.nav a').eq(1).addClass('main-nav-link__active curpoint-normal');
+        }
+    }
+
+
     // Search
     var $searchWrap = $('#search-form-wrap'),
         isSearchAnim = false,
@@ -135,20 +161,170 @@
         $container.removeClass('mobile-nav-on');
     });
 
-    //markdown nav
-    function hideMarkDownNav(){
-        $('#sidebar').hide();
-        $('#main').find('article').css('width','100%');
+    //设置导航全屏高度
+    if ($('#toc').length > 0) {
+        $('#toc').height($(window).height());
     }
-    function showMarkDownNav(){
-        $('#main').find('article').css('width','75%');
-    }
-    //当没有目录的时候隐藏掉
-    if($('.toc').text() == 'None'){
-        hideMarkDownNav();
-    }
-    //按钮切换
-    $('.sidebarBtn__hide').on('click',function(){
-        hideMarkDownNav();
+    $('.fix-sidebar-nav').on('click', function() {
+            if ($('.big-sidebar').hasClass('big-sidebar__show')) {
+                $('.big-sidebar').removeClass('big-sidebar__show');
+                $('.fix-sidebar-nav').removeClass('fix-sidebar-nav__active');
+            } else {
+                $('.big-sidebar').addClass('big-sidebar__show');
+                $('.fix-sidebar-nav').addClass('fix-sidebar-nav__active');
+            }
+        })
+        //markdown nav
+        // function hideMarkDownNav(){
+        //     $('#sidebar').hide();
+        //     $('#main').find('article').css('width','100%');
+        // }
+        // function showMarkDownNav(){
+        //     $('#main').find('article').css('width','75%');
+        // }
+        //当没有目录的时候隐藏掉
+        // if($('.toc').text() == 'None'){
+        //     hideMarkDownNav();
+        // }
+        //按钮切换
+        // $('.sidebarBtn__hide').on('click',function(){
+        //     hideMarkDownNav();
+        // });
+
+    //评论按钮点击
+    $('.fix-sidebar-comment').on('click', function() {
+        var ids = ['comment', 'comments', 'uyan_frame'];
+        for (var i = 0; i < ids.length; i++) {
+            if (document.getElementById(ids[i])) {
+                break;
+            }
+        }
+        var comment = document.getElementById(ids[i]);
+        $("html, body").animate({
+            scrollTop: $(comment).offset().top
+        }, 500, null);
     });
+
+    $('.fix-sidebar-share').on('click', function() {
+        var ids = ['baidu-share'];
+        for (var i = 0; i < ids.length; i++) {
+            if (document.getElementById(ids[i])) {
+                break;
+            }
+        }
+        var comment = document.getElementById(ids[i]);
+        $("html, body").animate({
+            scrollTop: $(comment).offset().top - $(window).height() / 2 + 30
+        }, 500, null);
+    });
+
+    var musicIsLoad = false;
+    $('.fix-sidebar-music').on('click', function() {
+        if (musicIsLoad) return; //防止重复点击加载多个播放器
+        musicIsLoad = true;
+        if ($('#musicComponent').length <= 0) return; //没有dom结构,return掉
+        var musicComponent = document.getElementById('musicComponent');
+        musicComponent.style.margin = '0'
+        musicComponent.style.padding = '0'
+        musicComponent.style.border = 'none'
+        musicComponent.style.width = '330px'
+        musicComponent.style.height = '450px'
+        musicComponent.style.overflow = 'hidden'
+        musicComponent.style.position = "absolute"
+        musicComponent.style.left = '-330px'
+        musicComponent.style.top = "0"
+        musicComponent.style.zIndex = 10
+        var frame = document.createElement('iframe');
+        frame.frameborder = 'no';
+        frame.border = "0"
+        frame.marginwidth = "0"
+        frame.marginheight = "0"
+        frame.width = 330
+        frame.height = 450
+        frame.src = "//music.163.com/outchain/player?type=0&id=700296152&auto=1&height=430"
+        document.getElementById('musicComponent').appendChild(frame);
+        frame.onload = function() {
+            musicIsLoad = false;
+            $('.fix-sidebar-music').addClass('fix-sidebar-music__active');
+            $('.fix-sidebar-music').off('click').on('click', function() {
+                if ($('.fix-sidebar-music').hasClass('fix-sidebar-music__active')) {
+                    $('.fix-sidebar-music').removeClass('fix-sidebar-music__active');
+                    $('#musicComponent').hide();
+                } else {
+                    $('.fix-sidebar-music').addClass('fix-sidebar-music__active');
+                    $('#musicComponent').show();
+                }
+            });
+        }
+    });
+    $('.fix-sidebar-search').on('click', function() {
+        $('.search-form-submit').trigger('click');
+        //@todo 点击会刷新页面
+    });
+
+    var menuPosition = [];
+    var sidebarLinks = [];
+    function initMenuPosition() {
+        if($('.article-entry').length <= 0 ) return;
+        var selectTag = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+        var tagArr = $('.article-entry').find(selectTag.join(','));
+        for (var i = 0, len = tagArr.length; i < len; i++) {
+            menuPosition.push({
+                target: tagArr[i],
+                y: $(tagArr[i]).offset().top
+            });
+        }
+        updateMenuPosition();
+    }
+    initMenuPosition();
+
+    function updateMenuPosition(){
+        // var menuPosition
+        var scrollTop = $(document).scrollTop();
+        var item = getItemFromMenuPosition(menuPosition,scrollTop);
+        sidebarLinks = $('.toc').find('a');
+        for(var i=0,len=sidebarLinks.length;i<len;i++){
+            if($(sidebarLinks[i]).attr('href').slice(1) == item.target.id){
+                $(sidebarLinks[i]).addClass('color-text__active');
+            }else{
+                $(sidebarLinks[i]).removeClass('color-text__active');
+            }
+        }
+    }
+
+    function getItemFromMenuPosition(items,value){
+        if(value <= items[0].y) return items[0];
+        if(value >= items[items.length-1].y) return items[items.length-1];
+        for(var i=0,len=items.length;i<len;i++){
+            if(value >= items[i].y && value < items[i+1].y){
+                return items[i];
+            }
+        }
+    }
+
+    function throttle(method, delay, duration) {
+        var timer = null,
+            begin = new Date();
+        return function() {
+            var context = this,
+                args = arguments,
+                current = new Date();;
+            clearTimeout(timer);
+            if (current - begin >= duration) {
+                method.apply(context, args);
+                begin = current;
+            } else {
+                timer = setTimeout(function() {
+                    method.apply(context, args);
+                }, delay);
+            }
+        }
+    }
+    var bigSidebar = $('.big-sidebar');
+    var scrollHandler = function(event) {
+        if(bigSidebar.hasClass('big-sidebar__show')){
+            updateMenuPosition();
+        }
+    }
+    $(document).scroll(throttle(scrollHandler, 100, 500));
 })(jQuery);
